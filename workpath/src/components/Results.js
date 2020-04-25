d64cf014adec8a5cbeda897ad690ac2077865e87
tstewart161 <1.61tstewart@gmail.com>
2020-04-25 17:00:54 -0400

    Working on form submission

--- workpath/src/components/Results.js
+++ workpath/src/components/Results.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
 class Results extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: '',
+            category: ''
+        }
+    }
+
+    getNewsArticles = (term, category) => {
+        let URL = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=yourkey`
+        let config = { 'Authorization': '27c7fabc-2398-4c3d-b85c-b1169ddb364a' }
+
+        axios.get(URL, { headers: config }).then(console.log())
+    }
 
     render() {
         return (

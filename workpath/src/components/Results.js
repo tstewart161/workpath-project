01fff725e1f8b5c10a451e3fb3178dc8c092a2b0
tstewart161 <1.61tstewart@gmail.com>
2020-04-26 12:29:28 -0400

    Adding date formatting

--- workpath/src/components/Results.js
+++ workpath/src/components/Results.js
@@ -2,10 +2,30 @@ import React from 'react';
 
 class Results extends React.Component {
 
+    componentDidMount() {
+        let date = new Date('2017-02-17T22:32:25.000Z');
+        let formatOptions = { 
+            day:    '2-digit', 
+            month:  '2-digit', 
+            year:   'numeric',
+            hour:   '2-digit', 
+            minute: '2-digit',
+            hour12: true 
+        };
+        let dateString = date.toLocaleDateString('en-US', formatOptions);
+        // => "02/17/2017, 11:32 PM"
+
+        dateString = dateString.replace(',', '') + " EST"
+        // convert to Fiji
+
+
+        console.log(dateString);
+    }
+
     renderArticleList = () => {
         // let term = this.props.term
         // let searched_articles = this.props.articles.filter(item => {
-        //     item.title.includes(term) || item.url.includes(term) || item.abstract.i})
+        //  item.title.includes(term) || item.url.includes(term) || item.abstract.i})
         return (
             <ul>
                 {this.props.articles.map((item, i) => (

d506d9ec39f954e205f1bbbe77aaf7bffff48fd7
tstewart161 <1.61tstewart@gmail.com>
2020-04-25 21:29:22 -0400

    working on rendering logic

--- workpath/src/components/Results.js
+++ workpath/src/components/Results.js
@@ -2,6 +2,21 @@ import React from 'react';
 import axios from 'axios';
 
 class Results extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0,
+            articles: []
+        }
+    }
+
+    componentWillReceiveProps() {
+        let rawArticles = this.getNewsArticles(this.props.category)
+        this.setState({
+            articles: rawArticles
+        }, console.log(this.state.articles))
+    }
 
     // Process:
     // use category to return articles from api
@@ -10,16 +25,23 @@ class Results extends React.Component {
     // render filtered article list
     getNewsArticles = (category) => {
         let URL = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=VAbwhpQE6koVHshxkEiy1fV2ZoiQDwny`
+        let articles = []
 
-        axios.get(URL).then((res) => { console.log(res.data.results) })
+        axios.get(URL).then((res) => { 
+            console.log(res.data.results)
+            articles = res.data.results
+            this.setState({
+                articles: articles
+            })
+        })
 
-        // return articles
+        return articles
     }
 
     renderArticleList = (term, category) => {
         console.log(term)
         console.log(category)
-        this.getNewsArticles(this.props.category)
+        // render the articles from state.
     }
 
     render() {
